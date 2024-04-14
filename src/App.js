1bc6ebdbbab92bdc5620985279e43934bdf353e9
AbhishekSaroch123 <abhishek@prodsmiths>
2024-04-14 20:44:21 +0530

    about page

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Error from "./pages/Error";
 import ForgotPassword from "./pages/ForgotPassword";
 import UpdatePassword from "./pages/UpdatePassword";
 import VerifyEmail from "./pages/VerifyEmail";
+import About from "./pages/About";
 function App() {
   return (
     <div className="w-screen min-h-screen bg-richblack-900 flex flex-col font-inter">
@@ -22,6 +23,7 @@ function App() {
         <Route path="/forgot-password" element={<ForgotPassword />}/>
         <Route path="/update-password" element={<UpdatePassword />}/>
         <Route path="/verify-email" element={<VerifyEmail />} /> 
+        <Route path="/about" element={<About />} /> 
         
       </Routes>
     </div>
---
+++ src/components/core/about/ContactFormSection.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ContactUsForm from '../../ContactPage.jsx/ContactUsForm'
+const ContactFormSection = () => {
+  return (
+    <div className='mx-auto'>
+        <h1>Get In Touch</h1>
+        <p>We'd love to hear you. Pls Fill Out This form</p>
+        <div>
+            <ContactUsForm />
+        </div>
+    </div>
+  )
+}
+
+export default ContactFormSection
