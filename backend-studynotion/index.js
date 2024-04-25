20c66739103a178cbd43611d6f9ec136f701584b
AbhishekSaroch123 <abhishek@prodsmiths>
2024-04-25 19:11:58 +0530

    updated origins in cors

--- backend-studynotion/index.js
+++ backend-studynotion/index.js
@@ -19,14 +19,20 @@ app.use(express.json());
 //database connect
 database.dbConnect();
 //middlewares
-
+const allowedOrigins = [
+	'http://localhost:3000',
+	'https://paathshala.vercel.app/'
+  ];
 app.use(cookieParser());
-app.use(
-	cors({
-		origin:"http://localhost:3000",
-		credentials:true,
-	})
-)
+app.use(cors({
+	origin: function (origin, callback) {
+	  if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+		callback(null, true);
+	  } else {
+		callback(new Error('Not allowed by CORS'));
+	  }
+	}
+  }));
 
 app.use(
 	fileUpload({
